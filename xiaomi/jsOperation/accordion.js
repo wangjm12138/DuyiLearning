e81980ae9057214667d9f27a746a981f12c7ade4
jackwang <373629168@qq.com>
2024-10-11 13:02:12 +0800

    accordion finish

--- xiaomi/jsOperation/accordion.js
+++ xiaomi/jsOperation/accordion.js
@@ -1,14 +1,71 @@
-(function(){
-        var h1 = document.querySelectorAll('.container h1');
-        console.log(h1);
-        for(var i = 0; i < h1.length; i++){
-            h1[i].addEventListener("click", function(){
-                var UlElment = this.nextElementSibling;
-                UlElment.style.display = "none";
-            })            
+
+var h2 = document.querySelectorAll('.menu h2');
+for(var i = 0; i < h2.length; i++){
+    h2[i].addEventListener("click", function(){
+        var beforeOpen = document.querySelector(".submenu[status=opened]")
+        if(beforeOpen){
+            closeSubmenu(beforeOpen);
         }
-        // h1.onclick = function(){
-        //     document.get
-        //     ul.style.height = "0px";
-        // };
-})();
+        var UlElment = this.nextElementSibling;
+        toggle(UlElment);
+    })            
+}
+
+
+var itemHeight = 30;
+
+// var testmenu = document.querySelector(".submenu");
+function openSubmenu(subMenu){
+    // submun state, open, close, play animation
+    // self-defined attribute
+    var status = subMenu.getAttribute("status");
+    if(status !== "close" && status) 
+        return;
+    subMenu.setAttribute("status", "playing");
+
+    createAnimation({
+        from: 0,
+        to: itemHeight * subMenu.children.length,
+        totalMS: 500,
+        onmove: function(height){
+            subMenu.style.height = height + "px";
+        },
+        onend: function(){
+            subMenu.setAttribute("status", "opened");
+        },
+    })
+}
+        
+function closeSubmenu(subMenu){
+    // submun state, open, close, play animation
+    // self-defined attribute
+    var status = subMenu.getAttribute("status");
+    if(status !== "opened") 
+        return;
+    subMenu.setAttribute("status", "playing");
+
+    createAnimation({
+        from: itemHeight * subMenu.children.length,
+        to: 0,
+        totalMS: 500,
+        onmove: function(height){
+            subMenu.style.height = height + "px";
+        },
+        onend: function(){
+            subMenu.setAttribute("status", "closed");
+        },
+    })
+}
+
+function toggle(subMenu){
+    // submun state, open, close, play animation
+    // self-defined attribute
+    var status = subMenu.getAttribute("status");
+    if(status === "playing"){
+        return;
+    } else if (status === "opened"){
+        closeSubmenu(subMenu);
+    } else {    // closed
+        openSubmenu(subMenu);
+    }
+}
