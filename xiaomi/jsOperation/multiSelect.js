f06f98faa45fe2860947c096bb023e25176f1314
jackwang <373629168@qq.com>
2024-10-14 08:53:05 +0800

    multiSelect completion

--- xiaomi/jsOperation/multiSelect.js
+++ xiaomi/jsOperation/multiSelect.js
@@ -1,24 +1,76 @@
 (function(){
-    var province_data = new Array('北京', '河北省', '河南省', '山东省', '山西省');
-    var city_data = new Array('北京市', '石家庄市', '郑州市', '济南市', '太原市');
-    var school_data = new Array('北京大学', '河北大学', '河南大学', '山东大学', '山西大学');
+    var province_data = {
+        '北京':'0001', 
+        '河北省':'0002', 
+        '河南省':'0003',
+    };
+    var city_data = {
+        '0001':{
+            '北京市':'001'
+        },
+        '0002':{
+            '石家庄市':'002',
+            '唐山市':'003',
+        },
+        '0003':{
+            '郑州市':'004',
+            '洛阳市':'005',
+        },
+    };
+    var school_data = {
+        '001':{
+            '北京大学':'0011',
+            '清华大学':'0012',
+        }, 
+        '002':{
+            '河北大学':'0021',
+            '河北工业大学':'0022',
+        },
+        '003':{
+            '河北师范大学':'0031',
+            '河北农业大学':'0032',
+        },
+        '004':{
+            '郑州大学':'0041',
+            '河南大学':'0042',
+        },
+        '005':{
+            '洛阳大学':'0051',
+            '洛阳师范大学':'0052',
+        },
+
+    };
     var province = document.querySelector('.container.province select');
     var city = document.querySelector('.container.city select');
     var school = document.querySelector('.container.school select');
 
     generateOption(province_data, province);
-    generateOption(city_data, city);
-    generateOption(school_data, school);
+
 
     province.addEventListener('change', function(){
         var index = province.selectedIndex;
-        
+        var value = province.options[index].value;
+        city.innerHTML = '';
+        school.innerHTML = '';
+        generateOption(city_data[value], city);
+        var index = city.selectedIndex;
+        var value = city.options[index].value;
+        generateOption(school_data[value], school);
+
+    });
+
+    city.addEventListener('change', function(){
+        var index = city.selectedIndex;
+        var value = city.options[index].value;
+        school.innerHTML = '';
+        generateOption(school_data[value], school);       
     });
 
     function generateOption(data, target){
-        for(var i = 0; i < data.length; i++){
+        for(var item in data){
             var option = document.createElement('option');
-            option.innerHTML = data[i];
+            option.innerText = item;
+            option.value = data[item];
             target.appendChild(option);
         }
     }
